9a67f345715ff5fb998068bb3fbe56966c7ed8a3
Albert Pangestu <stamina.left@gmail.com>
2021-11-15 13:22:02 +0700

    Setup the model for table view in string-array variable named 'names'.

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
