82d8e1851dbede0e2f2763e535fadbe109b03d92
Albert Pangestu <stamina.left@gmail.com>
2021-11-15 23:29:42 +0700

    Imported CoreData and renamed var from "names" into "people" with type array of NSManagedObject

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
                       return
                   }
             
-            self.names.append(nameToSave)
+            self.people.append(nameToSave)
             self.tableView.reloadData()
         }
         
@@ -54,13 +55,13 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
                                                  for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = people[indexPath.row]
         return cell
     }
     
